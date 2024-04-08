e8a682b473ba14f3dd4566322efc270de845ed23
sam <44710166+samharrell24@users.noreply.github.com>
2024-04-08 01:19:46 -0400

    Update sobel_filter_openmp.cpp

--- sobel/sobel_filter_openmp.cpp
+++ sobel/sobel_filter_openmp.cpp
@@ -11,7 +11,11 @@
 #include <sstream>
 #include <vector>
 #include <mpi.h>
+#include <chrono>
+#include <thread>
 #include <deque>
+#include <unistd.h>
+#include <iostream>
 
 namespace fs = std::filesystem;
 
@@ -91,6 +95,9 @@ void do_stuff(char const *filename, char const *outputfile){
         }
     }
 
+    std::cout << outputfile << "\n";
+
+
     stbi_write_bmp(outputfile, y, x, 1, data);
     stbi_image_free(data);
 }
@@ -99,12 +106,19 @@ int main(int argc, char *argv[]){
     int p, rank;
     const char delimeter='/';
     std::string path = "../video/snail_frames";
-    std::string output_path = "../video/snail_frames";
+    std::string output_path = "../video/snail_frames/out%d.bmp";
     std::deque<const char*> dq;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // MPI_Request req;
+    // MPI_Status stat;
+    int my_value[100];
+    char name[MPI_MAX_OBJECT_NAME];
+    int name_length;
+
 
     if (rank==0){
         for (const auto& entry : fs::directory_iterator(path)) {
@@ -115,17 +129,161 @@ int main(int argc, char *argv[]){
                 dq.push_back(fn);
             }   
         }
+        // 20 -> 1
 
-        std::cout << "Main Thing @ Rank: "<<rank<<"\n";
+        int dq_size = dq.size();
+
+        std::cout << "Main Thing @ Rank: "<<rank<<" dq_size: "<<dq_size<<" \n";
+        // std::cout << dq_size;
         MPI_Barrier(MPI_COMM_WORLD);
+
+        // while(dq_size > 0){
+        //     const char* file_bmp = dq.front();
+        //     dq.pop_front();
+        //     dq_size -= 1;
+        //     MPI_Bcast(&dq_size, 1, MPI_INT, 0, MPI_COMM_WORLD);    
+        // }
+
+        // 20,19,18,17 till 0 on all nodes without issue
+
+        // int t = dq_size;
+        int recv;
+        // while(int i = 0)
+        // while (t > 0){
+        //     int buffer_sent = t;
+        //     MPI_Send(&buffer_sent, 1, MPI_INT, SEND, 0, MPI_COMM_WORLD);
+        //     t-=1;
+        // }
+        int t = 0;
+        int buffer[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
+        MPI_Scatter(&buffer, 2, MPI_INT, &my_value, 1,MPI_INT,0,MPI_COMM_WORLD);
+        // MPI_
+        // while (t > 0) {
+            
+
+            // MPI_Request requests[7];
+            // MPI_Status statuses[7];
+            // for (int i=1;i<8;i++){
+                // int buffer_sent = t;
+                // MPI_Isend( &buffer_sent, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &req);
+                // std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+                // MPI_Ssend(&buffer_sent, 1, MPI_INT, RECEIVER, 0, MPI_COMM_WORLD);
+
+                
+                // MPI_Send(&buffer[i-1], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+                // t-=1;
+                
+            //     int MPI_Isend(const void* buffer,
+            //   int count,
+            //   MPI_Datatype datatype,
+            //   int recipient,
+            //   int tag,
+            //   MPI_Comm communicator,
+            //   MPI_Request* request);
+
+            //     int MPI_Recv(void* buffer,
+            //  int count,
+            //  MPI_Datatype datatype,
+            //  int sender,
+            //  int tag,
+            //  MPI_Comm communicator,
+            //  MPI_Status* status);
+
+                // MPI_Isend(&buffer[i], 1, MPI_INT, i, 6, MPI_COMM_WORLD,&requests[i-1]);
+                
+                // MPI_Recv()
+                // t-=1;
+                // std::cout << t<<std::endl;
+            // }
+            
+            // if(MPI_Waitall(7,requests,statuses) == MPI_SUCCESS){
+            //     t-=7;
+            //     std::cout << "see me"<<std::endl;
+            // }
+        // }
+        
+
+        // MPI_Send();
+        
+        // MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+
+        // Distribute file names among other ranks
+        // for (int dest_rank = 1; dest_rank < 12; ++dest_rank) {
+        //     int num_files_to_send = dq_size / (12 - 1);
+        //     int start_index = (dest_rank - 1) * num_files_to_send;
+        //     int end_index = dest_rank * num_files_to_send;
+        // if (dest_rank == 12 - 1) {
+        //     end_index = dq_size; // Ensure all files are distributed
+        // }
+        // for (int i = start_index; i < end_index; ++i) {
+        //     const char* file_bmp = dq;
+        //     MPI_Send(file_bmp, strlen(file_bmp)+1, MPI_CHAR, dest_rank, 0, MPI_COMM_WORLD);
+        // }   
     }
     else {
         MPI_Barrier(MPI_COMM_WORLD);
-        std::cout << "Other Thread @ Rank: "<<rank<<"\n";
-        const char* file_bmp = dq.front();
-        std::cout << "Do you see me? @ Rank: " << rank << "\n";
+        int comm_size;
+        MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+
+        // int received;
+        // int complete=1;
+        // while (true){
+        //     if (received <= 0){
+        //         exit;
+        //     }
+        //     MPI_Recv(&received, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // std::cout << "Other Thread @ Rank: "<<rank<<" recieved: "<<received<<" comm size: "<<comm_size<<"\n";
+        //     MPI_Send(&complete, 1, MPI_INT,0,6,MPI_COMM_WORLD)
+        
+        // }
+        MPI_Scatter(NULL, 2, MPI_INT, &my_value, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Get_processor_name(name,&name_length);
+
+        // do_stuff(f1.c_str(),o1.c_str());
+
+        std::cout << "Other Process on Node: " <<name<<" @ Rank: "<<rank<<" recieved: "<<my_value<<" comm size: "<<comm_size<<"\n";
+        // MPI_Request request;
+        // MPI_Irecv(&received, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &req);
+        // std::cout << "Other Thread @ Rank: "<<rank<<" recieved: "<<received<<" comm size: "<<comm_size<<"\n";
+
+        // MPI_Test(&req, &received, &stat);
+
+        // const char* file_bmp = dq.front();
+        // std::cout << file_bmp << "test";
+        // std::cout << "Do you see me? @ Rank: " << rank << "\n";
         // std::cout << "Do something Other Thread @ Rank: "<<rank<<"\n";
-        dq.pop_front();
+        // dq.pop_front();
+        // std::cout << "\t" << dq.size();
+        
+        // int dq_size;
+        // MPI_Bcast(&dq_size, 1, MPI_INT, 0, MPI_COMM_WORLD); // Need Broadcast on BOTH ENDS
+        // std::cout << "Other Thread @ Rank: "<<rank<<" with DQ Size: "<<dq_size<<"\n";
+
+        // while (dq_size > 0) {
+        //     MPI_Bcast(&dq_size, 1, MPI_INT, 0, MPI_COMM_WORLD); // Need Broadcast on BOTH ENDS
+        //     std::cout << "Other Thread @ Rank: "<<rank<<" with DQ Size: "<<dq_size<<"\n";
+        // }
+
+        // ofstream myfile;
+        // myfile.open ("example.txt");
+        // myfile << "Writing this to a file.\n";
+        // myfile.close();
+
+        // Receive file names from rank 0
+        // for (int i = 0; i < dq_size; ++i) {
+        //     char file_bmp[MAX_FILENAME_LENGTH]; // Define MAX_FILENAME_LENGTH accordingly
+        //     MPI_Recv(file_bmp, MAX_FILENAME_LENGTH, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //     // Process file_bmp here
+        // }
+
+        // for (int i = 0; i < dq_size; ++i) {
+        //     char file_bmp[MAX_FILENAME_LENGTH]; // Define MAX_FILENAME_LENGTH accordingly
+        //     MPI_Recv(file_bmp, MAX_FILENAME_LENGTH, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //     // Process file_bmp here
+        // }
+        // std::cout << "Other thread @ rank "<<rank<<" do something.";
+        // MPI_Wait(&request, MPI_STATUS_IGNORE);
 
         // std::string appended = file_bmp;
         // appended += ".bmp";
